5645075cd323ce4f56b195d76031ad56a989c15b
Marianne Linardos <mlinardos.edu@gmail.com>
2019-07-26 12:20:30 -0400

    Added foreach loop

--- ForLoopProj/Program.cs
+++ ForLoopProj/Program.cs
@@ -8,15 +8,48 @@ namespace ForLoopProj {
                     397, 915, 878, 674, 701, 379, 331, 804, 461, 459, 908, 373 };
             int total = 0;
 
-            for (int idx = 0; idx < ints.Length; idx++) {
-                /* total += ints[idx]; */
-                /* total += ints[idx] * 2; */
-                total += ints[idx]  * 3/4;
+           
+
+            /**** for 
+            //for (int idx = 0; idx < ints.Length; idx++) {
+            //    /* total += ints[idx]; */
+            //    /* total += ints[idx] * 2; */
+            //    total += ints[idx]  * 3/4;
+            //}
+
+            foreach (int Fred in ints) {        // type - 'int', made up variable - 'Fred', in - 'in'(always used), collection used - 'ints'
+                total += Fred;                 // body the same except 'anInt' replaces 'ints[idx] / does the same
             }
 
-            /* decimal average = total / (decimal) ints.Length;  */    //just for this one line, cast ints.Length as decimal
-            decimal average = total / ints.Length;
+
+            decimal average = total / (decimal) ints.Length;     //just for this one line, cast ints.Length as decimal
+            //decimal average = total / ints.Length;
             Console.WriteLine($"Total: {total}, Agerage: {average}");
+
+
+            // FOREACH LOOP //                                                        // USE IN PRS CAPSTONE
+            // Use foreach loops the most for collections //
+
+
+            /*** change names[] to all lower case ***/     //Uae variable to convert names / use foreach loop
+                                                           /***/
+            string[] names = { "Brailee", "Briana", "Marianne", "Maggie", "Glenn", "Alex", "Jesse", "Titus" };
+
+          
+
+            string allNames = "";                          // create empty string for collection/ plural
+
+            foreach (string name in names) {
+
+                string upperName = name.ToUpper();         // make upper case
+
+                allNames += upperName;                     //  same as allNames = allNames + upperName 
+                                                           // concatonates each name in collection with name.TuUpper               
+            }
+
+            Console.WriteLine(allNames);
+
+            /***/
 
         }
     }
